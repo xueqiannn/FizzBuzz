9a8bb035507dffbda80af6ac198249f2a2a0d2f8
Xueqian Li <XLi112@slb.com>
2023-11-01 14:15:04 +0800

    test: write FizzBuzzGame test

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -6,12 +6,26 @@ namespace FizzBuzzTest
     public class FizzBuzzGameTest
     {
         [Fact]
-        public void Test1()
+        public void Should_return_Fizz_when_CountOff_given_3()
         {
             FizzBuzzGame game = new FizzBuzzGame();
 
             //then
             Assert.NotNull(game);
+        }
+
+        [Fact]
+        public void Should_return_the_given_number_when_CountOff_given_a_normal_case_number()
+        {
+            //given
+            int number = 1;
+
+            //when
+            FizzBuzzGame game = new FizzBuzzGame();
+            string res = game.CountOff(number);
+
+            //then
+            Assert.Equal("1", res);
         }
     }
 }
